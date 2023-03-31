1447bf9e9fd2c7c0e31abbf1d2ec269e10bd8d54
nTropicGravity <bogdan.manescu.2015@gmail.com>
2023-03-31 17:39:01 +0300

    added collapsed menu feature

--- service-manager/service-manager-ui/src/app/header/navigation/navigation.component.ts
+++ service-manager/service-manager-ui/src/app/header/navigation/navigation.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HeaderService } from '../header.service';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {
-  isExpanded = true;
-  isLoggedIn = true;
+export class NavigationComponent implements OnInit {
+  @ViewChild('drawer', { static: false }) navigation: MatSidenav;
+  isExpanded: boolean;
+  isLoggedIn: boolean;
+
+  constructor(private headerService: HeaderService) {}
+
+  ngOnInit() {
+    this.isExpanded = this.headerService.isExpanded;
+    this.isLoggedIn = this.headerService.isLoggedIn;
+    this.headerService.onMenuEvent.subscribe((e) => (this.isExpanded = e));
+    this.headerService.onLogoutEvent.subscribe((e) => {
+      this.isLoggedIn = e;
+      this.navigation.close();
+    });
+  }
 }
