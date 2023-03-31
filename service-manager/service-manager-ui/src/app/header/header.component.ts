850a32708ebe6d2457b63b51d50acdd8a6499c19
nTropicGravity <bogdan.manescu.2015@gmail.com>
2023-03-31 11:51:21 +0300

    also added collapsed menu feature

--- service-manager/service-manager-ui/src/app/header/header.component.ts
+++ service-manager/service-manager-ui/src/app/header/header.component.ts
@@ -3,16 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   loggedIn: boolean = true;
 
-  onMenu() {
-    
-  }
+  onMenu() {}
 
-  onLogout() {
-    
-  }
+  onLogout() {}
 }
