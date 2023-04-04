2ff68a8a047d42685b5a437e9f90b64329584c6f
Valentin Serban <valentin.serban@ausy.com>
2023-04-04 10:25:27 +0300

    Added GlobalExceptionHandlingMiddleware in Api

---
+++ service-manager/service-manager-api/ServiceManager/ServiceManager.Domain/Models/Error.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServiceManager.Domain.Models
+{
+    public class Error
+    {
+        public string StatucCode { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
