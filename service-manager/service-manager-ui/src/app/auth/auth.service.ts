7842b8eaecfad2bb0b35bb41c820c5bd4a2094b7
nTropicGravity <bogdan.manescu.2015@gmail.com>
2023-04-07 15:00:10 +0300

    added login page

--- service-manager/service-manager-ui/src/app/auth/auth.service.ts
+++ service-manager/service-manager-ui/src/app/auth/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  isLoggedIn = false;
+  onAuthEvent = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  onLogout() {
+    this.isLoggedIn = false;
+    this.onAuthEvent.emit(this.isLoggedIn);
+    this.router.navigate(['/login']);
+  }
+
+  onLogin() {
+    this.isLoggedIn = true;
+    this.onAuthEvent.emit(this.isLoggedIn);
+    this.router.navigate(['/dashboard']);
+  }
 }
--- service-manager/service-manager-ui/src/app/auth/login/login.component.ts
+++ service-manager/service-manager-ui/src/app/auth/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HeaderService } from 'src/app/header/header.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.loading = true;
+      this.authService.onLogin();
+      this.loading = false;
+    }
+  }
+
+  initForm() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
 }
