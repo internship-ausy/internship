77bc1e9a5d1e2b488e422fbecef44f1ac2de52ce
tibi-ului <tiberiu.ului@ausy.com>
2023-04-12 15:08:30 +0300

    add create change password page

--- service-manager/service-manager-ui/src/app/auth/change-password/change-password.component.ts
+++ service-manager/service-manager-ui/src/app/auth/change-password/change-password.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChangePassword } from 'src/app/shared/models/changePassword.model';
+import { AuthResponseData, AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit {
+  changePasswordForm: FormGroup;
+  emailToReset!: string;
+  emailToken!: string;
+  changePassword: ChangePassword;
 
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.changePasswordForm = new FormGroup({
+      password: new FormControl('', [
+        Validators.required,
+        this.authService.passwordNotValid,
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
+
+    this.activatedRoute.queryParams.subscribe((value) => {
+      this.emailToReset = value['email'];
+      this.emailToken = value['token'];
+      console.log(this.emailToken);
+      console.log(this.emailToReset);
+    });
+  }
+
+  onSubmit(form: FormGroupDirective) {
+    if (!form.valid) {
+      this.changePassword.email = this.emailToReset;
+      this.changePassword.newPassword = this.changePasswordForm.value.password;
+      this.changePassword.confirmPassword =
+        this.changePasswordForm.value.confirmPassword;
+      this.changePassword.emailToken = this.emailToken;
+
+      this.authService.changePassword(this.changePassword).subscribe({
+        next: (resData) => {
+          this.router.navigate(['/login']);
+        },
+        error: (resData) => {
+          
+        }
+      });
+    } else {
+      this.changePasswordForm.reset();
+    }
+  }
 }
