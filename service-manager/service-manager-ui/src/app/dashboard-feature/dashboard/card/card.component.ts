799d186c7d6dcdd54824524199695e1b5f3f0245
Valentin Serban <valentin.serban@ausy.com>
2023-04-25 14:38:35 +0300

    Added card translations and logic

--- service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/card/card.component.ts
+++ service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/card/card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DashboardCard } from 'src/app/shared/models/dashboardCard.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() cardContent: DashboardCard = new DashboardCard();
+  
+  ngOnInit(): void {
 
+  }
+
+  onEdit() {
+    
+  }
+
+  onDelete() {
+    
+  }
 }
