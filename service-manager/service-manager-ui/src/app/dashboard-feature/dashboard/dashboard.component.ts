d952e9305e013a1581004b4322f521cf2ec3ed7f
Valentin Serban <valentin.serban@ausy.com>
2023-04-28 09:31:23 +0300

    Added the display logic

--- service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/dashboard.component.ts
+++ service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/dashboard.component.ts
@@ -10,22 +10,36 @@ import { StateDashboardService } from '../state-dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   reservations: Reservation[];
+  reservationsDisplayed: Reservation[];
+  reservationsPerPage = 6;
 
-  constructor(private dashboardService: DashboardService, private stateDashboardService: StateDashboardService) {
-    // dashboardService.getDashboardCards().subscribe({
-    //   next: (res) => {
-    //     this.reservations = res.data;
-    //     console.log(this.reservations);
-    //   }
-    // })
-  }
+  constructor(private stateDashboardService: StateDashboardService) {}
 
   ngOnInit(): void {
-    this.displayAddReservations();
+    this.displayReservations();
   }
 
-  displayAddReservations() {
+  displayReservations() {
     this.stateDashboardService.reservations$
-      .subscribe(reservations => this.reservations = reservations)
+      .subscribe(reservations => {
+        this.reservations = reservations;
+        this.reservations = this.reservations
+          .filter(r => {
+            return new Date(r.date) > new Date(Date());
+          })
+          .sort((a, b) => {
+            return new Date(a.date).getTime() - new Date(b.date).getTime();
+          });
+
+        if (this.reservations.length > this.reservationsPerPage) {
+            this.reservationsDisplayed = this.reservations.slice(0, 6);
+        } else {
+          this.reservationsDisplayed = this.reservations;
+        }
+      })
+  }
+
+  onSeeMore() {
+
   }
 }
