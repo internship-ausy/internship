4368110dad5f242a9145ccf76863f97550508029
tibi-ului <tiberiu.ului@ausy.com>
2023-05-03 23:54:33 +0300

    add styling to the scheduler

--- service-manager/service-manager-ui/src/app/dashboard-feature/schedule/schedule.component.ts
+++ service-manager/service-manager-ui/src/app/dashboard-feature/schedule/schedule.component.ts
@@ -35,7 +35,6 @@ export class ScheduleComponent implements OnInit {
     {
       start: '12:00',
       end: '13:00',
-      title: 'Lunch break',
       recurring: {
         repeat: 'weekly',
         weekDays: 'MO,TU,WE,TH,FR',
@@ -71,68 +70,40 @@ export class ScheduleComponent implements OnInit {
 
   myEvents: MbscCalendarEvent[] = [
     {
+      start: '2023-05-01T08:00',
+      end: '2023-05-01T17:00',
+      title: 'service',
+      resource: [1, 3],
+      color: '#35bb5a',
+    },
+    {
+      start: '2023-05-02T08:00',
+      end: '2023-05-02T17:00',
+      title: 'service',
+      resource: [1, 2, 3],
+      color: '#35bb5a',
+    },
+    {
       start: '2023-05-03T08:00',
       end: '2023-05-03T17:00',
       title: 'service',
-      resource: 1,
+      resource: [1, 2, 3],
       color: '#35bb5a',
     },
     {
-      start: '2023-05-03T11:00',
-      end: '2023-05-03T14:00',
-      title: 'break',
-      resource: 1,
-      color: '#913aa7',
-    },
-    {
-      start: '2023-04-30T10:00',
-      end: '2023-04-30T15:00',
-      title: 'Impact Training',
-      resource: [2, 3],
+      start: '2023-05-04T08:00',
+      end: '2023-05-04T17:00',
+      title: 'service',
+      resource: [1, 2, 3],
       color: '#35bb5a',
     },
     {
-      start: '2023-05-01T10:00',
-      end: '2023-05-01T15:00',
-      title: 'Impact Training',
-      resource: [2, 3],
+      start: '2023-05-05T08:00',
+      end: '2023-05-05T17:00',
+      title: 'service',
+      resource: [1, 2, 3],
       color: '#35bb5a',
-    },
-    {
-      start: '2023-05-03T08:30',
-      end: '2023-05-03T10:00',
-      title: 'Quick mtg. with Martin',
-      resource: 3,
-      color: '#913aa7',
-    },
-    // {
-    //   start: '2023-05-03T12:00',
-    //   end: '2023-05-03T13:00',
-    //   title: 'General orientation',
-    //   resource: [1, 2, 3],
-    //   color: '#a71111',
-    // },
-    {
-      start: '2023-05-04T10:00',
-      end: '2023-05-04T11:00',
-      title: 'Product team mtg.',
-      resource: [2, 3],
-      color: '#6e7f29',
-    },
-    {
-      start: '2023-05-05T14:00',
-      end: '2023-05-05T16:00',
-      title: 'Stakeholder mtg.',
-      resource: 1,
-      color: '#dcd234',
-    },
-    {
-      start: '2023-05-06T10:00',
-      end: '2023-05-06T14:00',
-      title: 'Innovation mtg.',
-      resource: [1, 2],
-      color: '#de3d83',
-    },
+    }
   ];
 
   myResources: MbscResource[] = [
@@ -143,7 +114,7 @@ export class ScheduleComponent implements OnInit {
     },
     {
       id: 2,
-      name: 'ws 3',
+      name: 'ws 2',
       color: '#61943D',
     },
     {
