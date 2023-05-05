51cb6bf38b5558bdb409ebd6038e06a11f4f3cd0
tibi-ului <tiberiu.ului@ausy.com>
2023-05-05 12:15:43 +0300

    add final touches

--- service-manager/service-manager-ui/src/app/dashboard-feature/schedule/schedule.component.ts
+++ service-manager/service-manager-ui/src/app/dashboard-feature/schedule/schedule.component.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
 setOptions({
   theme: 'windows',
   themeVariant: 'light',
-  clickToCreate: true,
+  clickToCreate: false,
   dragToCreate: false,
   dragToMove: false,
   dragToResize: false,
@@ -25,6 +25,8 @@ setOptions({
   encapsulation: ViewEncapsulation.None,
 })
 export class ScheduleComponent implements OnInit {
+  formatDate = formatDate;
+
   invalid = [
     {
       recurring: {
@@ -49,7 +51,6 @@ export class ScheduleComponent implements OnInit {
       },
     },
   ];
-  constructor(private http: HttpClient) {}
 
   view: MbscEventcalendarView = {
     schedule: {
@@ -62,98 +63,26 @@ export class ScheduleComponent implements OnInit {
     },
   };
 
-  // milestones = [{
-  //     date: '2023-05-01T00:00',
-  //     name: 'Project review',
-  //     color: '#f5da7b'
-  // }, {
-  //     date: '2023-05-02T00:00',
-  //     name: 'Product shipping',
-  //     color: '#acf3a3'
-  // }, {
-  //     date: '2023-05-04T00:00',
-  //     name: 'Cycle finish',
-  //     color: '#ff84a0'
-  // }];
-
-  myEvents: MbscCalendarEvent[] = [
-    {
-      start: '2023-05-01T08:00',
-      end: '2023-05-01T09:00',
-      title: 'service',
-      resource: [1, 3],
-      color: '#35bb5a',
-    },
-    {
-      start: '2023-05-01T09:00',
-      end: '2023-05-01T10:00',
-      title: 'service',
-      resource: [1, 3],
-      color: '#35bb5a',
-    },
-
-    {
-      start: '2023-05-02T08:00',
-      end: '2023-05-02T14:00',
-      title: 'service',
-      resource: [1, 2, 3],
-      color: '#35bb5a',
-    },
-    {
-      start: '2023-05-03T08:00',
-      end: '2023-05-03T17:00',
-      title: 'service',
-      resource: [1, 2, 3],
-      color: '#35bb5a',
-    },
-    {
-      start: '2023-05-04T08:00',
-      end: '2023-05-04T17:00',
-      title: 'service',
-      resource: [1, 2, 3],
-      color: '#35bb5a',
-    },
-    {
-      start: '2023-05-05T08:00',
-      end: '2023-05-05T17:00',
-      title: 'service',
-      resource: [1, 2, 3],
-      color: '#35bb5a',
-    },
-  ];
+  myEvents: MbscCalendarEvent[] = [];
 
   myResources: MbscResource[] = [
     {
       id: 1,
       name: 'ws 1',
-      color: '#61943D',
+      color: 'var(--tertiary-container)',
     },
     {
       id: 2,
       name: 'ws 2',
-      color: '#61943D',
+      color: 'var(--tertiary-container)',
     },
     {
       id: 3,
       name: 'ws 3',
-      color: '#61943D',
+      color: 'var(--tertiary-container)',
     },
   ];
+  constructor(private http: HttpClient) {}
 
-  formatDate = formatDate;
-
-  ngOnInit(): void {
-    this.http
-      .jsonp<MbscCalendarEvent[]>(
-        'https://trial.mobiscroll.com/resource-events/',
-        'callback'
-      )
-      .subscribe((resp) => {
-        //this.myEvents = resp;
-      });
-  }
-
-  // getTask(date: any): any {
-  //     return this.milestones.find((obj) => +new Date(obj.date) === +date) || {};
-  // }
+  ngOnInit(): void {}
 }
