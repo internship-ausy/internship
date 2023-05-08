dd1afec71f1b8f285e655b77406501b0c57dcdb6
raresc <rarescartuta@yahoo.com>
2023-05-08 07:23:06 +0300

    Added functionality for see-more-option

--- service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/dashboard.component.ts
+++ service-manager/service-manager-ui/src/app/dashboard-feature/dashboard/dashboard.component.ts
@@ -8,19 +8,24 @@ import { StateDashboardService } from '../state-dashboard.service';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  startIndex : number;
   reservations: Reservation[];
   reservationsDisplayed: Reservation[];
+  reservationCount : number;
   reservationsPerPage = 6;
+  nextReservations: Reservation[];
 
   constructor(private stateDashboardService: StateDashboardService) {}
 
   ngOnInit(): void {
     this.displayReservations();
+    console.log(this.reservationsDisplayed.length)
   }
 
   displayReservations() {
     this.stateDashboardService.reservations$.subscribe((reservations) => {
       this.reservations = reservations;
+      this.reservationCount = reservations.length;
       this.reservations = this.reservations
         .filter((r) => {
           return new Date(r.date) > new Date(Date());
@@ -37,5 +42,12 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  onSeeMore() {}
+  onSeeMore() {
+    {
+      this.startIndex = this.reservationsDisplayed.length;
+      this.nextReservations = this.reservations.slice(this.startIndex, this.startIndex + 6);
+      this.reservationsDisplayed = this.reservationsDisplayed.concat(this.nextReservations);
+      console.log(this.reservations.length)
+    }
+  }
 }
